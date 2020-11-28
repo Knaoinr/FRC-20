8ed32badef6ba6511e4e3b5cc1b9ac993848fc21
Knaoinr <knaoinr@gmail.com>
2020-11-27 19:31:25 -0500

    last updates!

--- src/main/java/frc/robot/commands/HelixTurn.java
+++ src/main/java/frc/robot/commands/HelixTurn.java
@@ -1,32 +1,45 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.subsystems.Subsystems;
+// import frc.robot.subsystems.Subsystems;
 
 /**
  * Turns the helix on.
  */
 public class HelixTurn extends Command {
 
+    /* This is just a sample command - there's no helix to turn in our imaginary robot.
+
+    The following are two examples of how you could write its constructor. In both cases, you have to call super("name") and requires(subsystem).
+    The timeout part isn't required, but allows you to end the command after a set amount of time.
+    */
+
     public HelixTurn() {
         super("HelixTurn");
-        requires(Subsystems.helix);
+        // requires(Subsystems.helix);
         setTimeout(300);
     }
 
     public HelixTurn(double Timeout) {
         super("HelixTurn");
-        requires(Subsystems.helix);
+        // requires(Subsystems.helix);
         setTimeout(Timeout);
     }
 
+    /*
+    Then, there will be up to 5 different methods: initialize, execute, isFinished, interrupted, and end.
+    If you don't need any of them, you can omit them (this happens most of the time).
+    Hover over their names below for a description - if nothing pops up, you might need to build the project first.
+    */
+
     @Override
     protected void initialize() {
-        Subsystems.helix.setHelixMotors(-0.8);
+        // Subsystems.helix.setHelixMotors(-0.8);
     }
 
     @Override
     protected void execute() {
+        // Usually, things would go here, but since you only need to turn on the motors once, there's nothing needed here!
     }
 
     @Override
@@ -35,13 +48,8 @@ public class HelixTurn extends Command {
     }
 
     @Override
-    protected void interrupted() {
-        Subsystems.helix.stopHelixMotors();
-    }
-
-    @Override
     protected void end() {
-        Subsystems.helix.stopHelixMotors();
+        // Subsystems.helix.stopHelixMotors();
     }
 
 }
--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -6,9 +6,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  * Warms up the flywheel, then starts shooting out power cells.
  */
 public class Shoot extends CommandGroup {
+
+    /*
+    Command groups are higher level instructions that execute one command after another.
+    The example is from 2020; to shoot a ball, we first got a ball to the top of the helix, then got the flywheel up to speed, then started shooting.
+    */
+
     public Shoot() {
         addSequential(new HelixTurn(), 0.3);
-        addSequential(new StartFlywheel());
-        addSequential(new HelixShoot());
+        // addSequential(new StartFlywheel());
+        // addSequential(new HelixShoot());
     }
 }
